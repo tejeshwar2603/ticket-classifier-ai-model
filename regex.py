b601a09d0bef2f4eeccf0472e64ff6224d0762e7
tejeshwar2603 <tejucool1234@gmail.com>
2026-05-03 00:42:34 +0530

    step-1-using-regex

--- regex.py
+++ regex.py
@@ -1,1 +1,17 @@
 #regular expressions
+
+def classify_log(log):
+    import re
+    if re.search(r'ERROR', log):
+        return 'Error'
+    elif re.search(r'WARNING', log):
+        return 'Warning'
+    elif re.search(r'INFO', log):
+        return 'Info'
+    else:
+        return 'Unknown'
+    
+# Example usage
+log_entry = "2024-06-01 12:00:00 ERROR Something went wrong"
+classification = classify_log(log_entry)
+print(f"The log entry is classified as: {classification}")
